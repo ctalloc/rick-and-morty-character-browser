277fcca2fe406f8c96934c1e823ff7fff850063c
ctalloc <cecilia.tallo@gmail.com>
2021-03-03 17:57:11 +0100

    First commit, added callToApi component.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../stylesheets/App.scss';
+import getDataFromApi from '../services/getDataFromApi'
+
+console.log(getDataFromApi());
+function App() {
+  return (
+    <p>Adiós mundo</p>
+  );
+}
+
+export default App;
---
+++ src/services/getDataFromApi.js
@@ -0,0 +1,18 @@
+const getDataFromApi = () => {
+    return fetch('https://rickandmortyapi.com/api/character')
+      .then(response => response.json())
+      .then(data => {
+          console.log(data);
+        return data.results.map((character) => {
+          return {
+            id: character.id,
+            name: character.name,
+            status: character.status,
+            species: character.species,
+            image :character.image,
+          };
+        });
+      });
+  };
+  export default getDataFromApi;
+  
