2a96c369a8decbb5551f841537e5dfc5ab292881
ctalloc <cecilia.tallo@gmail.com>
2021-03-03 18:37:36 +0100

    Paint characters list.

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,9 +1,11 @@
-const CharacterCard = props =>{
-    console.log(props);
-    return (
-      <>
-      <p>holis</p>
-      </>
-    )};
+const CharacterCard = (props) => {
+  console.log(props);
+  return (
+    <article>
+      <img src={props.chardata.image} alt={props.chardata.name} />
+      <h2>{props.chardata.name}</h2>
+    </article>
+  );
+};
 
 export default CharacterCard;
